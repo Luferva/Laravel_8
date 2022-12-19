9cbc08d67d768830b9c28c95248339d906af8ad7
Luferva <lfrv2005@gmail.com>
2022-12-19 17:14:43 -0300

    aula 21, concluida

--- arquivos/hdcevents/resources/views/welcome.blade.php
+++ arquivos/hdcevents/resources/views/welcome.blade.php
@@ -2,34 +2,43 @@
 
 @section('title', 'HDC Events')
 
-@section ('content')
+@section('content')
 
-<div id="search-container" classs="col-md-12">
-    <h1>Busque um evento</h1>
-    <form action="">
-        <input type="text" id="search" name="search" class="form-control" placeholder="Pesquise aqui">
-    </form>
-</div>
+    <div id="search-container" classs="col-md-12">
 
-<div id="events-container" class="col-md-12">
-    <h2>Proximos Eventos</h2>
-    <p class="subtitle">Veja as proximas listas</p>
-    <div id="cards-container" class="row">
-        @foreach($events as $event)
-        <div class="card col-md-3">
-            <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
-            <div class="card-body">
-                <p class="card-date">{{date('d/m/Y', strtotime($event->date)) }}</p>
-                <h5 class="card-title">{{$event->title}}</h5>
-                <p class="card-participants">X participante</p>
-                <a href="/events/{{$event->id}}" class="btn btn-primary">Saber mais</a>
-            </div>
-        </div>
-        @endforeach
-        @if(count($events) == 0)
-            <p>Não há eventos disponiveis</p>
-        @endif
+        <h1>Busque um evento</h1>
+        <form action="/" method="GET">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Pesquise aqui">
+        </form>
     </div>
-</div>
+
+    <div id="events-container" class="col-md-12">
+        @if ($search)
+            <h2>Buscando por: {{ $search }}</h2>
+        @else
+            <h2>Proximos Eventos</h2>
+            <p class="subtitle">Veja as proximas listas</p>
+        @endif
+
+
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">X participante</p>
+                        <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+            @if (count($events) == 0 && $search)
+                <p>Não foi possivel encontrar nenhum evento por "{{ $search }}"! <a href="/">Ver Todos</a></p>
+            @elseif(count($events) == 0)
+                <p>Não há eventos disponiveis</p>
+            @endif
+        </div>
+    </div>
 
 @endsection
