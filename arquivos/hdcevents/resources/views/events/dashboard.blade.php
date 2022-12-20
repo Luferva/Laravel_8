8b2bc00f3152e5fd05957dbfb5158c4eb1e45c16
Luferva <lfrv2005@gmail.com>
2022-12-20 17:53:19 -0300

    aula 26, concluida

--- arquivos/hdcevents/resources/views/events/dashboard.blade.php
+++ arquivos/hdcevents/resources/views/events/dashboard.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-    <div class="col-md-10 offset-md-1 dashborad-title-container">
+    <div class="col-md-10 offset-md-1 dashboard-title-container">
         <h1>Meus Eventos</h1>
     </div>
-    <div class="col-md-10 offset-md-1 dashborad-title-container">
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
         @if (count($events) > 0)
             <table class="table">
                 <thead>
@@ -18,20 +18,26 @@
                         <th scope="col">Ações</th>
                     </tr>
                 </thead>
-
                 <tbody>
                     @foreach ($events as $event)
                         <tr>
                             <td scropt="row">{{ $loop->index + 1 }}</td>
                             <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                             <td>0</td>
-                            <td><a href="#">Editar</a><a href="#">Apagar</a></td>
+                            <td>
+                                <a href="#" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon>Editar</a>
+                                <form action="/events/{{$event->id}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon> Apagar</a>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
         @else
-            <p>Você ainda não tem eventos, <a href="/events/create">Criar Eventos</a></p>
+            <p>Você ainda não tem eventos, acesse: <a href="/events/create">Criar Eventos</a></p>
         @endif
     </div>
 
