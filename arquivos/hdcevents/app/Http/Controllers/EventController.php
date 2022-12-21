507aed5ac369bc845fe2ee69760cab612f2a5e61
Luferva <lfrv2005@gmail.com>
2022-12-21 17:57:36 -0300

    aula 30,concluida. Curso completado

--- arquivos/hdcevents/app/Http/Controllers/EventController.php
+++ arquivos/hdcevents/app/Http/Controllers/EventController.php
@@ -81,9 +81,25 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        //Para verificar se o usuário já esta participando desse evento
+        $user = auth()->user();
+
+        $hasUserJoined = false;
+
+        if($user){
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id){
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner'=>$eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner'=>$eventOwner, 'hasUserJoined' =>$hasUserJoined]);
     }
 
     public function dashboard(){
@@ -144,11 +160,23 @@ class EventController extends Controller
 
         $user = auth()->user();
 
-        $user->eventsAsParticipant()->attach($id);
+        $user->eventsAsParticipant()->attach($id); //attach() metódo que faz a ligação
 
         $event = Event::findOrFail($id);
 
-        return redirect('/dashboard')->with('msg','Sua presença esta confirmada no evento' . $event->title);
+        return redirect('/dashboard')->with('msg','Sua presença esta confirmada no evento: ' . $event->title);
+    }
+
+    public function leaveEvent($id){
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id); //detach() metódo que remove a ligação
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg','Sua presença esta cancelada no evento: ' . $event->title);
+
     }
 
     
--- arquivos/hdcevents/resources/views/events/show.blade.php
+++ arquivos/hdcevents/resources/views/events/show.blade.php
@@ -20,6 +20,8 @@
                 <p class="event-ower">
                     <ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }}
                 </p>
+                
+                @if(!$hasUserJoined)
                 <form action="/events/join/{{ $event->id }}" method="POST">
                     @csrf
                     <a href="/events/join/{{ $event->id}}" 
@@ -30,6 +32,9 @@
                         Confirmar Presença
                     </a>
                 </form>
+                @else
+                    <p class="already-joined-msg">Você já esta participando deste evento!</p>
+                @endif
                 <h3>O evento conta com:</h3>
                 <ul id="itens-list">
                     @foreach ($event->itens as $iten)
